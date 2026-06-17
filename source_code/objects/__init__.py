706856f98c586dc2d5f1fe5d83cba3d65e4dbf66
CDHS <caohoangson572006@gmail.com>
2026-06-17 16:36:54 +0700

    thêm docstring

--- source_code/objects/__init__.py
+++ source_code/objects/__init__.py
@@ -1,1 +1,4 @@
-# Khai báo package nội bộ objects
+"""
+Package tầng thực thể đối tượng (Object Layer).
+Định nghĩa cấu trúc dữ liệu của các thực thể trong hệ thống: sách, người dùng, phiếu mượn.
+"""
