daa8fc40668a0ad08c0f527fd26fdbd15aa688ed
CDHS <caohoangson572006@gmail.com>
2026-06-17 19:26:20 +0700

    thêm chi tiết về waitingqueue

--- source_code/structure/__init__.py
+++ source_code/structure/__init__.py
@@ -1,5 +1,5 @@
 """
 Package tầng cấu trúc dữ liệu (Structure Layer).
 Chứa các cấu trúc dữ liệu tự cài đặt vận hành trên RAM:
-BookHashMap, TransactionList, PriorityQueue, UserArray.
+BookHashMap, TransactionList, PriorityQueue, UserArray, WaitingQueue.
 """
