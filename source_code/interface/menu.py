358f37e2a77fa9d379d51a5d32757c4264ed2aaf
Hoàng Minh Sơn <tuyenmocson@gmail.com>
2026-06-19 00:01:46 +0700

    Update menu.py

--- source_code/interface/menu.py
+++ source_code/interface/menu.py
@@ -272,8 +272,8 @@ def admin_manage_loans(hash_map, dll, user_array, waiting_queue):
             u_id = input("Mã độc giả: ").strip()
             b_id = input("Mã sách: ").strip().upper()
             
-            success, msg, fee = loan_manager.process_return(hash_map, dll, user_array, u_id, b_id, waiting_queue)
-
+            success, msg, fee = loan_manager.process_return(hash_map, dll, user_array, waiting_queue, u_id, b_id)
+            
             print(f"\nHệ thống: {msg}")
             if fee > 0: print(f"⚠️ THU PHẠT: {fee} VNĐ (Trễ hạn)")
             if success: _trigger_save(hash_map, dll, user_array, waiting_queue)
